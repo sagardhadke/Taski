ac60f99d9a5897b5441d6efeb649e6fd4a356237
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-19 17:27:26 +0530

    Update Home page Code

--- lib/View/Home.dart
+++ lib/View/Home.dart
@@ -39,76 +39,91 @@ class _MyHomeState extends State<MyHome> {
       ),
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         SizedBox(height: 35),
+
+        // ExpansionTile(
+        //   title: Text("data"),
+        //   children: [
+        //     Text("data"),
+        //     Text("data"),
+        //     Text("data"),
+        //   ],
+        // ),
         // Text("data")
         Expanded(
           child: ListView.builder(
             itemCount: 15,
             itemBuilder: (context, index) {
               return Slidable(
-        endActionPane: ActionPane(
-          motion: DrawerMotion(),
-          children: [
-            SlidableAction(
-              onPressed: (context) {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog.adaptive(
-                        title: Text("Do you want to Update Your Note!"),
-                        content: Text("Update Your Notes"),
-                        actions: [
-                          ElevatedButton(
-                              onPressed: () {}, child: Text("Update"))
-                        ],
-                      );
-                    });
-              },
-              backgroundColor: Color(0xFF21B7CA),
-              foregroundColor: Colors.white,
-              icon: Icons.add_box_outlined,
-              label: 'Update',
-            ),
-            SlidableAction(
-              onPressed: (context) {},
-              backgroundColor: Color(0xFF7BC043),
-              foregroundColor: Colors.white,
-              icon: Icons.done_all_outlined,
-              label: 'Done',
-            ),
-          ],
-        ),
-        startActionPane: ActionPane(motion: DrawerMotion(), children: [
-          SlidableAction(
-            onPressed: (context) {},
-            backgroundColor: Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            icon: Icons.delete,
-            label: 'Delete',
-          ),
-        ]),
-        child: Container(
-          alignment: Alignment.topLeft,
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color(0xFFFFFFFF),
-          ),
-          height: 100,
-          width: double.infinity,
-          child: Column(
-            children: [
-              Text(
-                "Title : This is first TODO",
-                style: GoogleFonts.iceberg(
-                    textStyle:
-                        TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
-              ),
-              Text(
-                "Description : it's a description",
-                style: GoogleFonts.iceberg(textStyle: TextStyle(fontSize: 21)),
-              )
-            ],
-          ),
-        ));
+                endActionPane: ActionPane(
+                  motion: DrawerMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (context) {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog.adaptive(
+                                title: Text("Do you want to Update Your Note!"),
+                                content: Text("Update Your Notes"),
+                                actions: [
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text("Update"))
+                                ],
+                              );
+                            });
+                      },
+                      backgroundColor: Color(0xFF21B7CA),
+                      foregroundColor: Colors.white,
+                      icon: Icons.add_box_outlined,
+                      label: 'Update',
+                    ),
+                    SlidableAction(
+                      onPressed: (context) {},
+                      backgroundColor: Color(0xFF7BC043),
+                      foregroundColor: Colors.white,
+                      icon: Icons.done_all_outlined,
+                      label: 'Done',
+                    ),
+                  ],
+                ),
+                startActionPane: ActionPane(motion: DrawerMotion(), children: [
+                  SlidableAction(
+                    onPressed: (context) {},
+                    backgroundColor: Color(0xFFFE4A49),
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete,
+                    label: 'Delete',
+                  ),
+                ]),
+                child: ExpansionTile(
+                  title: Text("data",
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.circle_outlined,
+                        color: Colors.green,
+                      ),
+                      title: Text("First Note",
+                          style: TextStyle(
+                              fontSize: 21, fontWeight: FontWeight.bold)),
+                    ),
+                    Text(
+                      "Description : it's a description",
+                      style: GoogleFonts.iceberg(
+                          textStyle: TextStyle(fontSize: 21)),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.date_range_outlined,
+                        color: Colors.green,
+                      ),
+                      title: Text("First Note", style: TextStyle(fontSize: 21)),
+                    ),
+                  ],
+                ),
+              );
             },
           ),
         )
